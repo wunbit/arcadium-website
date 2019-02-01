96989a70990c693fd39d85510ed3e4a3d4078112
Carlos Oporto <carlosoporto@gmail.com>
2019-02-01 23:51:15 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,5 +7,12 @@ function crypterio_child_enqueue_parent_styles() {
 	wp_enqueue_style( 'crypterio-style', get_template_directory_uri() . '/style.css', array( 'bootstrap' ), CRYPTERIO_THEME_VERSION, 'all' );
 	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'crypterio-layout' ), CRYPTERIO_THEME_VERSION, 'all' );
 
+}
 
+// Remove WP Bakery Page Builder meta generator
+add_action('wp_head', 'myoverride', 1);
+function myoverride() {
+  if ( class_exists( 'Vc_Manager' ) ) {
+    remove_action('wp_head', array(visual_composer(), 'addMetaData'));
+  }
 }
