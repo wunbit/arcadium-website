a0a46e2a8339df5dc062a5239a576fb9153ac16c
Carlos Oporto <carlosoporto@gmail.com>
2019-02-13 22:56:36 +0100

    Change 404 error page

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php crypterio_get_header(); ?>
+	<div class="page_404">
+		<div class="bottom">
+			<div class="container">
+				<h1>404</h1>
+			</div>
+			<div class="bottom_wr">
+				<div class="container">
+					<div class="media">
+						<div class="media-body media-middle">
+							<h3><?php esc_html_e( 'The page you are looking for does not exist.', 'crypterio' ); ?></h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>
